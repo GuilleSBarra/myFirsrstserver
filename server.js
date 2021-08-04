357340bd575cfa4dfb2db8d85ae9dcf2728c6fb1
unknown <you@example.com>
2021-08-04 15:22:30 -0300

    almuerzo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const server = express();
+const path = require('path');
 
 server.get('/',(req,res)=>{
     res.send ('Hola Mundo!!!')
@@ -9,8 +10,9 @@ server.get('/guille',(req,res)=>{
     res.send ('Hola Universo!!!')
 })
 
-
-
+server.get('/home',(req,res)=>{
+    res.sendFile(path.join(__dirname,'.view/home.html'))
+})
 
 server.listen(3001,() => {
     console.log('Server is running in the port : 3001')
